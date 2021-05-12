b1d81bf8c936509b6f83b2eac98da8ae72e0a4e3
Mustafa Salih <37256744+MustafaSalih1993@users.noreply.github.com>
2021-05-12 16:46:48 +0300

    getting data programmatically (#56)
    * getting loadavg programmatically
    
    * added missing uptime in rsblock.yml
    
    * using clock_gettime syscall to get uptime

--- src/utils/load_average.rs
+++ src/utils/load_average.rs
@@ -1,12 +1,16 @@
 use crate::config::CONFIG;
 use crate::types::ThreadsData;
-use nix::sys::sysinfo;
+
+use nix::libc::{c_double, c_int, getloadavg};
 
 pub fn get_load_avg() -> ThreadsData {
-    let load = sysinfo::sysinfo().unwrap().load_average().0;
+    let mut data: [c_double; 3] = [0f64; 3];
+    unsafe { getloadavg(data.as_mut_ptr(), data.len() as c_int) };
+    let [load, _, _] = data;
     let data = format!(
         "  {}  {:.2}  {}",
         CONFIG.loadavg.icon, load, CONFIG.seperator
     );
+
     ThreadsData::LoadAvg(data)
 }
